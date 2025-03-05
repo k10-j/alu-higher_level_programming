eb84e4b5453574a176a0543c11e9199e6c8c39ea
k10-j <j.iriza@alustudent.com>
2025-03-05 12:59:42 -0800

    program that prints the result of the addition of all arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,10 +2,12 @@
 
 import sys
 
+
 def main():
     argv = sys.argv[1:]  # Exclude the script name itself
     total = sum(int(arg) for arg in argv)  # Convert to integers and sum
     print(total)
+
 
 if __name__ == "__main__":
     main()
