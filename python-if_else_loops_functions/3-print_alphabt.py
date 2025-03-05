2620dd95e0489ef9da862fff78794f0f474bf7df
k10-j <j.iriza@alustudent.com>
2025-03-05 02:09:25 -0800

    prints the ASCII alphabet, in lowercase

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(i) for i in range(97, 123) if i != 101 and i != 113))
-
+print("{}".format("".join(chr(i) for i in range(97, 123) if i not in (101, 113))), end="")
