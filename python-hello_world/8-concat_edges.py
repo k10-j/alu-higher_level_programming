d79c0be66fa4120a229b550ed35737ee7464423b
k10-j <j.iriza@alustudent.com>
2025-03-07 06:15:18 -0800

    to print object-oriented programming with Python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an amazing programming language"
-print(str[39:67] + str[-6:-1] + str[:6])
+str = "Python is an intrepreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clean syntax"
+str = str[39:67] + str[-22:-17] + str[:6]
+print(str)
