90875d3783bac599d8d1795fdc1e0c3320518d69
k10-j <j.iriza@alustudent.com>
2025-03-11 10:10:17 -0700

    scrip that reads a text file (UTF8) and prints it to stdout

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+"""Write a function that reads a text file (UTF8) and prints it to stdout:"""
+
 
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its contents to stdout."""
-    try:
-        with open(filename, encoding="utf-8") as f:
-            print(f.read(), end="")
-    except FileNotFoundError:
-        pass
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
